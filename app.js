272a31441cb893c0792ac63b5263707a2cf85778
AkhiSharmin <akhisharmin100@gmail.com>
2024-02-18 22:11:09 +0600

    done

--- app.js
+++ app.js
@@ -4,25 +4,57 @@
 //     main();
 // }
 
-function main() {
+// function main() {
+//     const root = document.getElementById('root');
+//     const btn = document.getElementById('change-btn');
+
+//     btn.addEventListener('click', function (e) {
+//         const bgColor = generateRGAColor()
+//         root.style.backgroundColor = bgColor;
+//     })
+// }
+
+
+// //step 2 random color generator function
+
+// function generateRGAColor() {
+//     const red = Math.floor(Math.random() * 255)
+//     const green = Math.floor(Math.random() * 255)
+//     const blue = Math.floor(Math.random() * 255)
+
+//     return `rgba(${red}, ${green}, ${blue})`
+// }
+
+// main()
+
+
+
+
+
+
+
+
+
+function changeColor() {
     const root = document.getElementById('root');
     const btn = document.getElementById('change-btn');
 
     btn.addEventListener('click', function (e) {
         const bgColor = generateRGAColor()
-        root.style.backgroundColor = bgColor;
+        root.style.backgroundColor = bgColor
     })
 }
 
 
-//step 2 random color generator function
-
 function generateRGAColor() {
-    const red = Math.floor(Math.random() * 255)
-    const green = Math.floor(Math.random() * 255)
-    const blue = Math.floor(Math.random() * 255)
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
 
-    return `rgba(${red}, ${green}, ${blue})`
+    return `rgba(${red} , ${green}, ${blue})`
 }
 
-main()
+changeColor()
+
+
+
