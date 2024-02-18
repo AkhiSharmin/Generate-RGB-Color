5315471fc08400a173646488dd3edd47fe489be0
AkhiSharmin <akhisharmin100@gmail.com>
2024-02-18 21:38:36 +0600

    color genarate done

---
+++ app.js
@@ -0,0 +1,28 @@
+// steps 1
+
+// window.onload = () => {
+//     main();
+// }
+
+function main() {
+    const root = document.getElementById('root');
+    const btn = document.getElementById('change-btn');
+
+    btn.addEventListener('click', function (e) {
+        const bgColor = generateRGAColor()
+        root.style.backgroundColor = bgColor;
+    })
+}
+
+
+//step 2 random color generator function
+
+function generateRGAColor() {
+    const red = Math.floor(Math.random() * 255)
+    const green = Math.floor(Math.random() * 255)
+    const blue = Math.floor(Math.random() * 255)
+
+    return `rgba(${red}, ${green}, ${blue})`
+}
+
+main()
